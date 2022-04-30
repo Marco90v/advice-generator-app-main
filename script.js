a6d53b7a8fedc91297ae74cda3bf237638432f50
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-04-30 05:20:30 -0400

    diseño de escritorio y api

---
+++ script.js
@@ -0,0 +1,11 @@
+const api = "https://api.adviceslip.com/advice";
+const num = document.querySelector("#advice");
+const frase = document.querySelector("#frase");
+async function random(){
+    const rest = await fetch(api).then(response=>response.json());
+    const {id, advice} = rest.slip;
+    num.innerHTML = "ADVICE #" + id;
+    frase.innerHTML = advice;
+}
+// random()
+window.onload = random();
